f2bcaedfe85f4d76b85dd49f12e6da0f319b61c1
Souvik Sinha <souvik@aphroecs.com>
2018-06-21 17:45:21 +0530

    link set

--- src/script.js
+++ src/script.js
@@ -4,3 +4,20 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello World";
 }
+
+$(document).ready(function(){
+    $('#search').keyup(function(){
+      $('#result').html('');
+      var searchField = $('#search').val();
+      var expression = new RegExp(searchField, "i");
+      $.getJSON('data.json', function(data){
+        $.each(data, function(key, value){
+          if(value.name.search(expression) != -1 || value.location.search(expression) != -1)
+          {
+            $("#result").append('<li class="list-group-item"><span class="text-muted"> '+value.name+'</span></li>')
+          }
+        });
+      });
+
+    });
+  });
